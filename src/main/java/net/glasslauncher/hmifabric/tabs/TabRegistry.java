c53c1cb8de2c47b015aeeb22b493323d26fba3e1
mineLdiver <aabesedin@mail.ru>
2023-04-25 03:04:45 +0500

    Update to latest StAPI

--- src/main/java/net/glasslauncher/hmifabric/tabs/TabRegistry.java
+++ src/main/java/net/glasslauncher/hmifabric/tabs/TabRegistry.java
@@ -3,22 +3,39 @@ package net.glasslauncher.hmifabric.tabs;
 import com.mojang.serialization.Lifecycle;
 import net.glasslauncher.hmifabric.HowManyItems;
 import net.glasslauncher.hmifabric.TabUtils;
+import net.minecraft.client.gui.screen.container.ContainerBase;
 import net.minecraft.item.ItemInstance;
-import net.modificationstation.stationapi.api.registry.Identifier;
-import net.modificationstation.stationapi.api.registry.Registry;
-import net.modificationstation.stationapi.api.registry.RegistryKey;
-import net.modificationstation.stationapi.api.registry.SimpleRegistry;
+import net.modificationstation.stationapi.api.registry.*;
 
 import java.util.*;
 
 public class TabRegistry extends SimpleRegistry<Tab> {
+    private static final Tab EMPTY = new Tab(ModID.MINECRAFT, 0, 0, 0, 0, 0) {
+        @Override
+        public ItemInstance getTabItem() {
+            return null;
+        }
+
+        @Override
+        public ItemInstance[][] getItems(int index, ItemInstance filter) {
+            return new ItemInstance[0][];
+        }
+
+        @Override
+        public void draw(int x, int y, int recipeOnThisPageIndex, int cursorX, int cursorY) {}
+
+        @Override
+        public Class<? extends ContainerBase> getGuiClass() {
+            return null;
+        }
+    };
     public static final RegistryKey<Registry<Tab>> KEY = RegistryKey.ofRegistry(HowManyItems.MODID.id("tabs"));
-    public static final TabRegistry INSTANCE = Registry.create(KEY, new TabRegistry(), Lifecycle.experimental());
+    public static final TabRegistry INSTANCE = Registries.create(KEY, new TabRegistry(), registry -> EMPTY, Lifecycle.experimental());
 
     public List<Tab> tabOrder = new ArrayList<>();
 
     public TabRegistry() {
-        super(KEY, Lifecycle.experimental(), null);
+        super(KEY, Lifecycle.experimental(), false);
     }
 
     /**
